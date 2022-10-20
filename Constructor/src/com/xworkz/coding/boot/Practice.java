75b9af23a3637dac6322c31950b2e14f2e7d0749
unknown <natarajs.xworkz@gmail.com>
2022-10-20 12:31:24 +0530

    java

---
+++ Constructor/src/com/xworkz/coding/boot/Practice.java
@@ -0,0 +1,12 @@
+package com.xworkz.coding.boot;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("checking.......");
+
+	}
+
+}
